13144d8628f7bb4199d4ad74dc19b2998b216a8c
DichEtaCom <etacom.bel@gmail.com>
2015-08-07 10:34:22 +0100

    1st dev commit

---
+++ SimpleClassExample/A_test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class A_test
+    {
+        public A_test()
+        {
+            
+        }
+    }
+}
